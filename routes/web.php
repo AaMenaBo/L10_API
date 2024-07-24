5bce85003231d201d8addc9712fed1f425b44806
Aaron Mena <aamenabo@est.utn.ac.cr>
2024-07-24 13:54:39 -0600

    bug fix

--- routes/web.php
+++ routes/web.php
@@ -12,20 +12,23 @@ Route::get('/', function () {
 
 
 //Rutas de Tareas
-Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-Route::get('/tasks/create', [TaskController::class, 'create']);
+Route::get('/tasks', [TaskController::class, 'index'])
+    ->name('tasks.index')
+    ->middleware('auth');
+Route::get('/tasks/create', [TaskController::class, 'create'])
+    ->middleware('auth');
 Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show')->middleware('auth')->can('view', 'task');
 Route::post('/tasks', [TaskController::class, 'store']);
-Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit')->middleware('auth')->can('update', 'task');      
+Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit')->middleware('auth')->can('update', 'task');
 Route::put('/tasks/{task}', [TaskController::class, 'update']);
 Route::put('/tasks/complete/{task}', [TaskController::class, 'complete'])->name('tasks.complete');
 Route::delete('/tasks/{task}', [TaskController::class, 'delete'])->name('tasks.delete');
 
 //Rutas de Login y Register
-Route::get('/register', [RegisterUserController::class, 'create'])->name('register.create');    
+Route::get('/register', [RegisterUserController::class, 'create'])->name('register.create');
 Route::post('/register', [RegisterUserController::class, 'store'])->name('register.store');
 
-Route::get('/login', [SessionController::class, 'create'])->name('login.create');
+Route::get('/login', [SessionController::class, 'create'])->name('login');
 Route::post('/login', [SessionController::class, 'store'])->name('login.store');
 Route::get('/logout', [SessionController::class, 'destroy'])->name('logout');
 Route::post('/logout', [SessionController::class, 'destroy'])->name('logout');
